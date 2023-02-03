eb9f82674d963bf09086a7bbc1f91cd8d2af56ae
Pedro Mesquita <pedro.mesquita@casacontrolada.com.br>
2023-02-03 09:14:28 -0300

    adds basic configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'src/styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title> React Avançado - BoilerPlate</title>
+				<link rel="shortcut icon" href="/public/next.svg" />
+				<link rel="apple-touch-icon" href="/public/next.svg" />
+				<meta
+					name="description"
+					content="A simple project Starter to work with Typescript, React,
+				 Next JS, and Styled Components"
+				/>
+			</Head>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</>
+	)
 }
