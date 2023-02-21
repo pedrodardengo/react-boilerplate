0b9c82a9d9588aa469ea67ca65e030262627fb1b
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-02-20 22:49:14 -0300

    updates storybook to 7.0

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
 	testEnvironment: 'jsdom',
-	testPathIgnorePatterns: ['<rootDir>/node_modules/', '/.next/'],
+	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 	collectCoverage: true,
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts(x)?'],
+	collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 	modulePaths: ['<rootDir>/src/'],
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+	},
+	moduleNameMapper: {
+		'^styled-components':
+			'<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+		'^.+\\.svg$': 'jest-svg-transformer'
 	}
 }
